dc71f4752c5210b0e75db4813b243f953af12820
Patrick Snowden <patrickbsnowden@outlook.com>
2024-08-22 21:04:58 -0700

    updated fetch

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,5 +1,5 @@
 export async function fetch_data(url: string) {
-  const res = await fetch(url);
+  const res = await fetch(url, { next: { revalidate: 0 } });
   if (!res.ok) {
     throw new Error("Failed to fetch");
   }
